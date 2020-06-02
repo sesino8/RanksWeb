be89fa5adfb095496c22345fb18f8d27cd72e546
sesino8 <tonibermejovalenzuela@gmail.com>
2020-06-03 00:09:01 +0200

    Tocando cosas ya en JS, cuidado con el c

--- public/js/buscar.js
+++ public/js/buscar.js
@@ -1,12 +1,18 @@
 var categorias = new Array;
+var webs = new Array;
+var detalles = new Array;
+var descriptionWebs = new Array;
+var description = new Array;
+
 
 
 
 window.onload = function () {
+	var elementDresciption = new Array;
 
-	 x = document.getElementById("login");
-	 y = document.getElementById("register");
-	 z = document.getElementById("btn");
+	x = document.getElementById("login");
+	y = document.getElementById("register");
+	z = document.getElementById("btn");
 
 
 
@@ -18,28 +24,65 @@ window.onload = function () {
 		mainNav.classList.toggle('active');
 	});
 
+
+	webs = document.getElementsByClassName("web");
+
+	for (let i = 0; i < webs.length; i++) {
+		detalles.push(webs[i].children);
+
+	}
+
+	for (let i = 0; i < detalles.length; i++) {
+		for (let j = 0; j < detalles[i].length; j++) {
+			//Cogemos los enlaces para sacarles cada URL de cada web para después a la hora de filtrar 
+			//que podamos clasificarlas sin problema
+			if (detalles[i][j].tagName == 'A') {
+				elementDresciption.push(detalles[i][j].href);
+				console.log(detalles[i][j].href);
+
+			} else {
+				elementDresciption.push(detalles[i][j].innerText);
+				console.log(detalles[i][j].innerText);
+
+			}
+
+		}
+		descriptionWebs.push(elementDresciption);
+		//Reiniciamos array para que no se sumen todos los elementos
+		elementDresciption = new Array;
+
+
+	}
+
+	console.log(descriptionWebs)
+
 }
 
-function init(){
+function init() {
 
 
 }
 
+//Funcion que rellena el array de categorias
+function categoria() {
+	console.log("fsajsa");
+
+}
 
 //FORM REGISTER
-function register(){
+function register() {
 	x.style.left = "-400px";
 	y.style.left = "50px";
 	console.log(document.getElementsByClassName("button-box")[0].offsetWidth);
 
-	if(document.getElementsByClassName("button-box")[0].offsetWidth >= "220"){
+	if (document.getElementsByClassName("button-box")[0].offsetWidth >= "220") {
 		z.style.left = "110px";
-	}else{
+	} else {
 		z.style.left = "85px";
 	}
 }
 
-function login(){
+function login() {
 	x.style.left = "50px";
 	y.style.left = "450px";
 	z.style.left = "0";
